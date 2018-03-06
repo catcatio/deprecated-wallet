916382540955f8323281ebec10be39eb077bc944
Todsaporn (katopz) Banjerdkit <katopz@gmail.com>
2018-03-06 18:44:24 +0700

    feat(web): Working POC account linking

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(8080, () => console.log('Example app listening on port 3000!'))
+require('dotenv/config')
+require('./app')
